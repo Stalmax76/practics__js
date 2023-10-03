5d5e50ca0b7d3d6abd83a8c54eeea3deb7b1e4f2
Stalmax76 <68605477+Stalmax76@users.noreply.github.com>
2023-10-03 21:38:53 +0200

    second test

--- src/script.js
+++ src/script.js
@@ -1,24 +1,36 @@
-try {
-  const numberOfFilms = +prompt('How many movies do you wathes', '');
-  console.log(numberOfFilms);
-  const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    private: false,
-  };
+const numberOfFilms = +prompt('How many movies do you wathes', '');
 
-  const a = prompt('What was the last movie you watched?', '');
-  const b = prompt('How would you rate the movie you watched?', '');
-  const c = prompt('What was the last movie you watched?', '');
-  const d = prompt('How would you rate the movie you watched?', '');
-  console.log(a);
-  console.log(b);
-  personalMovieDB.movies[a] = b;
-  personalMovieDB.movies[c] = d;
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  private: false,
+};
 
-  console.log(personalMovieDB);
-} catch (error) {
-  console.log(error);
+for (let i = 0; i < 2; i++) {
+  const a = prompt('What was the last movie you watched?', ''),
+    b = prompt('How would you rate the movie you watched?', '');
+
+  if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+    console.log('Thanks, your data is good!');
+  } else {
+    console.log('your answer is  incorect! Try again! ');
+    i--;
+  }
 }
+const key = personalMovieDB.count;
+console.log(`key is: ${key}`);
+
+if (key < 10) {
+  console.log('Watched quite a few films!');
+} else if (key >= 10 || key < 30) {
+  console.log('You are a classic viewer!');
+} else if (key >= 30) {
+  console.log('You are a film buff!');
+} else {
+  console.log('An error has occurred!');
+}
+
+console.log(personalMovieDB);
