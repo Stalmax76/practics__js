c0e6630420e4e91610b8cf8a6b7a5e7b607f2fe2
Stalmax76 <68605477+Stalmax76@users.noreply.github.com>
2023-10-04 20:04:35 +0200

    added functions for first task

--- src/script.js
+++ src/script.js
@@ -1,36 +1,63 @@
-const numberOfFilms = +prompt('How many movies do you wathes', '');
+let numberOfFilms;
+function start() {
+  numberOfFilms = +prompt('How many movies do you wathes', '');
+  while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+    numberOfFilms = +prompt('How many movies do you wathes', '');
+  }
+}
+start();
 
 const personalMovieDB = {
   count: numberOfFilms,
   movies: {},
   actors: {},
   genres: [],
-  private: false,
+  privat: false,
 };
 
-for (let i = 0; i < 2; i++) {
-  const a = prompt('What was the last movie you watched?', ''),
-    b = prompt('How would you rate the movie you watched?', '');
+function rememberMyFilms() {
+  for (let i = 0; i < 2; i++) {
+    const a = prompt('What was the last movie you watched?', ''),
+      b = prompt('How would you rate the movie you watched?', '');
 
-  if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-    personalMovieDB.movies[a] = b;
-    console.log('Thanks, your data is good!');
-  } else {
-    console.log('your answer is  incorect! Try again! ');
-    i--;
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+      personalMovieDB.movies[a] = b;
+      console.log('Thanks, your data is good!');
+    } else {
+      console.log('your answer is  incorect! Try again! ');
+      i--;
+    }
   }
 }
-const key = personalMovieDB.count;
-console.log(`key is: ${key}`);
+rememberMyFilms();
 
-if (key < 10) {
-  console.log('Watched quite a few films!');
-} else if (key >= 10 || key < 30) {
-  console.log('You are a classic viewer!');
-} else if (key >= 30) {
-  console.log('You are a film buff!');
-} else {
-  console.log('An error has occurred!');
+function detectPersonalLevel() {
+  const key = personalMovieDB.count;
+
+  if (key < 10) {
+    console.log('Watched quite a few films!');
+  } else if (key >= 10 || key < 30) {
+    console.log('You are a classic viewer!');
+  } else if (key >= 30) {
+    console.log('You are a film buff!');
+  } else {
+    console.log('An error has occurred!');
+  }
+}
+detectPersonalLevel();
+
+function showMyDB(hidden) {
+  if (!hidden) {
+    console.log(personalMovieDB);
+  }
 }
 
-console.log(personalMovieDB);
+function writeYourGenres() {
+  for (let i = 0; i < 3; i++) {
+    personalMovieDB.genres[i] = prompt(
+      `your favorite genre number ${i + 1} is: `
+    );
+  }
+}
+writeYourGenres();
+showMyDB(personalMovieDB.privat);
